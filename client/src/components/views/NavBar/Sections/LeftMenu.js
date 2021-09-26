1f7c1d8aed0e906d90ae9190b5e67071517fa739
sk <1724271@donga.ac.kr>
2021-09-26 22:00:07 +0900

    ADD Booking

--- client/src/components/views/NavBar/Sections/LeftMenu.js
+++ client/src/components/views/NavBar/Sections/LeftMenu.js
@@ -1,9 +1,28 @@
-import React from 'react';
-import { Menu } from 'antd';
+import React, { useEffect, useState } from 'react';
+import { Menu, Input } from 'antd';
+import { API_KEY, API_URL } from '../../../Config';
 const SubMenu = Menu.SubMenu;
 const MenuItemGroup = Menu.ItemGroup;
 
 function LeftMenu(props) {
+
+  const [Movies, setMovies] = useState([]);
+  
+  useEffect(() => {
+    const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+    fetchMovies(endpoint)
+  }, []);
+
+  const fetchMovies = (endpoint) => {
+    fetch(endpoint)
+    .then(response => response.json())
+    .then(response => {
+      console.log(response.results)
+      setMovies([...Movies,...response.results])
+    })
+  }
+
+
   return (
     <Menu mode={props.mode}>
     <Menu.Item key="mail">
@@ -12,8 +31,12 @@ function LeftMenu(props) {
     <Menu.Item key="favorite">
       <a href="/favorite">Favorite</a>
     </Menu.Item>
-   
+    <Menu.Item key="booking">
+      <a href="/booking">Booking</a>
+    </Menu.Item>
   </Menu>
+
+  
   )
 }
 
